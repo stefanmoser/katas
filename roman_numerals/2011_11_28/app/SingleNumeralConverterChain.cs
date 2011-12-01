4b03b18f89e3437f3091675bc69f7c45bd8a1a74
Stefan Moser <stefanm@iqmetrix.com>
2011-12-01 10:30:18 -0800

    day 3

---
+++ roman_numerals/2011_11_28/app/SingleNumeralConverterChain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace app
+{
+    public class SingleNumeralConverterChain
+    {
+        private static SingleNumeralConverter chain_head;
+
+        static SingleNumeralConverterChain()
+        {
+            List<RomanNumeral> numerals = new List<RomanNumeral>();
+            numerals.Add(RomanNumeral.One);
+            numerals.Add(RomanNumeral.Five);
+            numerals.Add(RomanNumeral.Ten);
+            numerals.Add(RomanNumeral.Fifty);
+
+            chain_head = null;
+
+            foreach (var numeral in numerals)
+            {
+                chain_head = new SingleNumeralConverter(numeral, chain_head);
+            }
+        }
+
+        public static IConvertSingleNumerals first_converter
+        {
+            get { return chain_head; }
+        }
+    }
+}
